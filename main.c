9f9d342e6443058689033471885ef1d9e263064d
Murilll <murilosertorio25@gmail.com>
2023-03-27 20:02:35 -0300

    ._>

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+// Exercício 1 (XP 7): Faça um programa em C (F.U.P) que pergunta um valor em metros e imprime o correspondente em decímetros, centímetros e milímetros.
+
+/*
+int main(int argc, char *argv[]) {
+	int metro = 0;
+	printf("Insira um valor em metros para ver o ocorrespondente em decímetros, centímetros e milímetros: \n");
+	scanf("%i", &metro);
+	
+	int decimetro = metro * 10;
+	int centimetros = metro * 100;
+	int milimetros = metro * 1000;
+	
+	printf("\nMetro: %i", metro);
+	printf("\nDecímetro: %i", decimetro);
+	printf("\nCentímetros: %i", centimetros);
+	printf("\nMilímetros: %i", milimetros);
+}
+*/
+
+
+// Exercício 2 (XP 10): F.U.P que imprime uma tabela com a tabuada de 1 a 9.
+
+/*
+int main(int argc, char *argv[]) {
+	for (int i = 0; i < 11; i++)
+	{
+		printf("\n");
+		for (int j = 0; j < 11; j++)
+		{
+			int tabuada = i * j;
+			printf("%i X %i = %i\n", i, j, tabuada);
+		}
+	}
+}
+/*
+
+
